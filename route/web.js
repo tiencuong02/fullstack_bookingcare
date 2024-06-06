327614cbadc4b52391fd82fc1033f2eaa61003f6
Cuong <chutiencuong1234567890@gmail.com>
2024-06-06 23:55:25 +0700

    fullstackproject

---
+++ route/web.js
@@ -0,0 +1,16 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  router.get("/about", homeController.getAboutPage);
+  router.get('/hoidanit',(req, res) =>{
+    return res.send('Hello world with hoidanit')
+  });
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
