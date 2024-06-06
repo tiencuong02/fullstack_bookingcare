901fd7e5f739cf85f4f7860fccc57872ef327021
Cuong <chutiencuong1234567890@gmail.com>
2024-06-07 02:35:34 +0700

    7/6/24

--- seeders/20240606164539-demo-user.js
+++ seeders/20240606164539-demo-user.js
@@ -2,23 +2,17 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'example@example.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    
   }
 };
