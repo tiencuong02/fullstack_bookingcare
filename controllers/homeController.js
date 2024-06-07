4f4eb92f11c7d615acb7b91cba76a9c7f5f7466b
Cuong <chutiencuong1234567890@gmail.com>
2024-06-08 03:10:57 +0700

    crud

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -15,7 +15,18 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) =>{
     return res.render('test/about.ejs')
 }
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = (req, res) =>{
+    console.log(req.body);
+    return res.send('post crud from server')
+}
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage : getAboutPage
+    getAboutPage : getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
--- route/web.js
+++ route/web.js
@@ -6,9 +6,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
-  router.get('/hoidanit',(req, res) =>{
-    return res.send('Hello world with hoidanit')
-  });
+  router.get("/crud", homeController.getCRUD);
+  router.post('/post-crud', homeController.postCRUD);
 
   return app.use("/", router);
 };
