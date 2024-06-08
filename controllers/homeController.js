1a3b1e35b15e2289a61cffbe7e7ea2bc7069ba6a
Cuong <chutiencuong1234567890@gmail.com>
2024-06-08 16:33:57 +0700

    CRUD sevices

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,6 @@
 
-import db from '../models/index'
+import db from '../models/index';
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try{
     let data = await db.User.findAll();
@@ -20,9 +21,10 @@ let getCRUD = (req, res) => {
     return res.render('crud.ejs')
 }
 
-let postCRUD = (req, res) =>{
-    console.log(req.body);
-    return res.send('post crud from server')
+let postCRUD = async(req, res)  =>{
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message)
+    return res.send('post crud from server');
 }
 module.exports = {
     getHomePage: getHomePage,
